325c2fb4f3b585c286393d925683336ae9dd496b
Kevin Castro <cas20057@byui.edu>
2024-02-01 15:29:44 -0700

    installed qr reader package and implemented it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
+
 import { QRCodeSVG } from 'qrcode.react';
+import { QrReader } from 'react-qr-reader';
 
 export default function Home() {
+  const [data, setData] = useState('No result');
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className='container'>
       <QRCodeSVG value='8237483ughjbqusyr36y' />
+
+      {visible && (
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.text);
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          style={{ width: '100%' }}
+        />
+      )}
+
+      <button onClick={() => setVisible(true)}>Scan me</button>
+
+      <p>{data}</p>
     </div>
   );
 }
